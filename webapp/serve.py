140c55e6e68c4db23c1a10f25a245bf6a90497ce
Ben Keller <malzraa@gmail.com>
2013-09-28 23:36:34 -0400

    Finished the first "demo" version of the website.  Looks good so far.

--- webapp/serve.py
+++ webapp/serve.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python
 from flask import Flask, render_template, url_for
+import random
+import os
 from twitter import *
 app = Flask(__name__)
+MY_TWITTER_CREDS = os.path.expanduser('~/.velotweet_oauth')
 
 def grab_power():
-	return 0
+	return random.randint(0,500)
 
-def grab_rpm():
-	return 0
+def grab_speed():
+	return random.randint(0,50)
+
+def grab_distance():
+	return random.randint(0,500)
+
+def munge_statuses(status):
+	return {'user':status['user']['screen_name'], 'text':status['text']}
 
 def get_tweets(hashtags):
-	return 0
+	oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+	twitter = Twitter(auth=OAuth(oauth_token, oauth_secret,
+	"pnOTHwklWj75l45HSF650g", "JyWoD0TgqE7ocSGlXNdHdKSpSmfijWS5LnikvVY"))
+	tweets = twitter.search.tweets(q=hashtags, count=5, result_type='recent')
+	return [munge_statuses(status) for status in tweets['statuses']]
 
 @app.route('/')
 def render_wall():
-	tags = []
+	tags = "@THAATCoop"
 	staticurls = {'css':url_for('static', filename='wall.css'),
 			'thaat':url_for('static', filename='thaat.png'),
 			'thinkhaus':url_for('static', filename='thinkhaus.png')}
 	return render_template('wall.html', 
-			static=staticurls, rpm=grab_rpm(), 
+			static=staticurls, speed=grab_speed(), 
 			power=grab_power(), tweets=get_tweets(tags),
-			event="#HIVEX")
+			distance=grab_distance(), event="#HIVEX")
 
 if __name__ == "__main__":
+	random.seed()
 	app.run(debug=True)
